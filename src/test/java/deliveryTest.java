fd8f5c26f26e98d2dcf5f4398627d7c49d1129d7
Pavel <Test@email.ru>
2023-05-25 19:22:14 +0400

    fix tests

--- src/test/java/deliveryTest.java
+++ src/test/java/deliveryTest.java
@@ -18,27 +18,33 @@ public class deliveryTest {
     @Test
     void happyPathTest() {
 //        Configuration.holdBrowserOpen = true;
-        open("http://localhost:9999");
-        $("[data-test-id=city] input").setValue("Казань");
-        $("[data-test-id=date] input").setValue(generateDate(5, "dd.MM.yyyy"));
+        String date = generateDate(5, "dd.MM.yyyy");
+        open("http://localhost:7777");
+        $("[data-test-id=city] input").setValue("Саратов");
+        $(byText("Саратов")).shouldBe(Condition.visible).click();
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(date);
         $("[data-test-id=name] input").setValue("Павел Цыганков");
         $("[data-test-id=phone] input").setValue("+79876542345");
         $("[data-test-id=agreement]").click();
         $$("button").find(Condition.exactText("Забронировать")).click();
         $(byText("Успешно!")).shouldBe(Condition.visible, Duration.ofMillis(15000));
+        $x("//*[@class='notification__content']").shouldHave(Condition.exactText("Встреча успешно забронирована на " + date), Duration.ofMillis(15000));
     }
 
     @Test
     void happyPathTest2() {
 //        Configuration.holdBrowserOpen = true;
-        open("http://localhost:9999");
-        $("[data-test-id=city] input").setValue("Ка");
-        $("[data-test-id=city] input").shouldBe(Condition.visible, Duration.ofMillis(1000)).sendKeys(Keys.DOWN, Keys.DOWN, Keys.DOWN, Keys.DOWN, Keys.DOWN, Keys.ENTER);
+        String date = generateDate(7, "dd.MM.yyyy");
+        open("http://localhost:7777");
+        $("[data-test-id=city] input").setValue("Са");
+        $(byText("Саратов")).shouldBe(Condition.visible).click();
         $("[data-test-id=date] input").shouldBe(Condition.visible, Duration.ofMillis(1000)).sendKeys(Keys.DOWN, Keys.DOWN, Keys.LEFT, Keys.LEFT, Keys.LEFT, Keys.ENTER);
         $("[data-test-id=name] input").setValue("Павел Цыганков");
         $("[data-test-id=phone] input").setValue("+79876542345");
         $("[data-test-id=agreement]").click();
         $$("button").find(Condition.exactText("Забронировать")).click();
         $(byText("Успешно!")).shouldBe(Condition.visible, Duration.ofMillis(15000));
+        $x("//*[@class='notification__content']").shouldHave(Condition.exactText("Встреча успешно забронирована на " + date), Duration.ofMillis(15000));
     }
 }
